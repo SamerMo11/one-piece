fe4c4541e859162cfccf15c52afe08b8ed7f166f
Samer Mohamed <140319729+SamerMo11@users.noreply.github.com>
2024-02-14 19:26:09 +0200

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,236 @@
+let btn = document.getElementsByClassName('btn');
+let btn22 = document.getElementsByClassName('btn22');
+let btn33 = document.getElementsByClassName('btn33');
+let btn44 = document.getElementsByClassName('btn44');
+let btn55 = document.getElementsByClassName('btn55');
+let btn66 = document.getElementsByClassName('btn66');
+let circle = document.getElementsByClassName('circle');
+let first = document.getElementsByClassName('first');
+let firstp = document.getElementsByClassName('first-p');
+let second = document.getElementsByClassName('second');
+let secondp = document.getElementsByClassName('second-p');
+let third = document.getElementsByClassName('third');
+let thirdp = document.getElementsByClassName('third-p');
+let fourth = document.getElementsByClassName('fourth');
+let fourthp = document.getElementsByClassName('fourth-p');
+let fifth = document.getElementsByClassName('fifth');
+let fifthp = document.getElementsByClassName('fifth-p');
+let sexth = document.getElementsByClassName('sexth');
+let sexthp = document.getElementsByClassName('sexth-p');
+
+
+for(let a=0 ; a<btn.length; a++){
+    btn[a].onclick = function(){
+        circle[a].style.cssText=`
+        transform: rotate(0deg);`;
+        first[a].style.cssText=`
+        opacity: 1; `;
+        firstp[a].style.cssText=`
+        display: flex; `;
+
+        second[a].style.cssText=`
+        opacity: 0;`;
+        secondp[a].style.cssText=`
+        display:none;`;
+
+        third[a].style.cssText=`
+        opacity: 0;`;
+        thirdp[a].style.cssText=`
+        display:none;`;
+
+        fourth[a].style.cssText=`
+        opacity: 0;`;
+        fourthp[a].style.cssText=`
+        display:none;`;
+
+        fifth[a].style.cssText=`
+        opacity: 0;`;
+        fifthp[a].style.cssText=`
+        display:none;`;
+
+        sexth[a].style.cssText=`
+        opacity: 0;`;
+        sexthp[a].style.cssText=`
+        display:none;`;
+    }
+}
+for(let q=0 ; q<btn22.length; q++){
+    btn22[q].onclick=function () {
+        circle[q].style.cssText=`
+        transform: rotate(360deg);`;
+
+        first[q].style.cssText=`
+        opacity: 0;`;
+        firstp[q].style.cssText=`
+        display:none;`;
+
+        second[q].style.cssText=`
+        opacity: 1;`;
+        secondp[q].style.cssText=`
+        display:flex;`;
+
+        third[q].style.cssText=`
+        opacity: 0;`;
+        thirdp[q].style.cssText=`
+        display:none;`;
+
+        fourth[q].style.cssText=`
+        opacity: 0;`;
+        fourthp[q].style.cssText=`
+        display:none;`;
+
+        fifth[q].style.cssText=`
+        opacity: 0;`;
+        fifthp[q].style.cssText=`
+        display:none;`;
+
+        sexth[q].style.cssText=`
+        opacity: 0;`;
+        sexthp[q].style.cssText=`
+        display:none;`;
+    }
+}
+for(let s=0 ; s<btn33.length; s++){
+    btn33[s].onclick=function () {
+        circle[s].style.cssText=`
+        transform: rotate(0deg);`;
+
+        first[s].style.cssText=`
+        opacity: 0;`;
+        firstp[s].style.cssText=`
+        display:none;`;
+
+        second[s].style.cssText=`
+        opacity: 0;`;
+        secondp[s].style.cssText=`
+        display:none;`;
+
+        third[s].style.cssText=`
+        opacity: 1;`;
+        thirdp[s].style.cssText=`
+        display:flex;`;
+
+        fourth[s].style.cssText=`
+        opacity: 0;`;
+        fourthp[s].style.cssText=`
+        display:none;`;
+
+        fifth[s].style.cssText=`
+        opacity: 0;`;
+        fifthp[s].style.cssText=`
+        display:none;`;
+
+        sexth[s].style.cssText=`
+        opacity: 0;`;
+        sexthp[s].style.cssText=`
+        display:none;`;
+    }
+}
+for(let f=0 ; f<btn44.length; f++){
+    btn44[f].onclick=function () {
+        circle[f].style.cssText=`
+        transform: rotate(360deg);`;
+        
+        first[f].style.cssText=`
+        opacity: 0;`;
+        firstp[f].style.cssText=`
+        display:none;`;
+
+        second[f].style.cssText=`
+        opacity: 0;`;
+        secondp[f].style.cssText=`
+        display:none;`;
+
+        third[f].style.cssText=`
+        opacity: 0;`;
+        thirdp[f].style.cssText=`
+        display:none;`;
+
+        fourth[f].style.cssText=`
+        opacity: 1;`;
+        fourthp[f].style.cssText=`
+        display:flex;`;
+
+        fifth[f].style.cssText=`
+        opacity: 0;`;
+        fifthp[f].style.cssText=`
+        display:none;`;
+
+        sexth[f].style.cssText=`
+        opacity: 0;`;
+        sexthp[f].style.cssText=`
+        display:none;`;
+    }
+}
+for(let x=0 ; x<btn55.length; x++){
+    btn55[x].onclick=function () {
+        circle[x].style.cssText=`
+        transform: rotate(0deg);`;
+        
+        first[x].style.cssText=`
+        opacity: 0;`;
+        firstp[x].style.cssText=`
+        display:none;`;
+
+        second[x].style.cssText=`
+        opacity: 0;`;
+        secondp[x].style.cssText=`
+        display:none;`;
+
+        third[x].style.cssText=`
+        opacity: 0;`;
+        thirdp[x].style.cssText=`
+        display:none;`;
+
+        fourth[x].style.cssText=`
+        opacity: 0;`;
+        fourthp[x].style.cssText=`
+        display:none;`;
+
+        fifth[x].style.cssText=`
+        opacity: 1;`;
+        fifthp[x].style.cssText=`
+        display:flex;`;
+
+        sexth[x].style.cssText=`
+        opacity: 0;`;
+        sexthp[x].style.cssText=`
+        display:none;`;
+    }
+}
+for(let z=0 ; z<btn66.length; z++){
+    btn66[z].onclick=function () {
+        circle[z].style.cssText=`
+        transform: rotate(360deg);`;
+        
+        first[z].style.cssText=`
+        opacity: 0;`;
+        firstp[z].style.cssText=`
+        display:none;`;
+
+        second[z].style.cssText=`
+        opacity: 0;`;
+        secondp[z].style.cssText=`
+        display:none;`;
+
+        third[z].style.cssText=`
+        opacity: 0;`;
+        thirdp[z].style.cssText=`
+        display:none;`;
+
+        fourth[z].style.cssText=`
+        opacity: 0;`;
+        fourthp[z].style.cssText=`
+        display:none;`;
+
+        fifth[z].style.cssText=`
+        opacity: 0;`;
+        fifthp[z].style.cssText=`
+        display:none;`;
+
+        sexth[z].style.cssText=`
+        opacity: 1;`;
+        sexthp[z].style.cssText=`
+        display:flex;`;
+    }
+}
---
+++ op.js
@@ -0,0 +1,353 @@
+window.addEventListener('scroll',reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i = 0; i < reveals.length ; i++){
+
+    let windowheight = window.innerHeight;
+    let revealtop = reveals[i].getBoundingClientRect().top;
+    let revealpoint = 100;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+// ----------------------------------------------------------
+window.addEventListener('scroll',bottom);
+
+function bottom(){
+  var bottoms = document.querySelectorAll('.bottom');
+
+  for(var b = 0; b < bottoms.length ; b++){
+
+    let windowheight = window.innerHeight;
+    let revealtopppp = bottoms[b].getBoundingClientRect().top;
+    let revealpointttt = 100;
+
+    if(revealtopppp < windowheight - revealpointttt){
+      bottoms[b].classList.add('active');
+    }
+    else{
+      bottoms[b].classList.remove('active');
+    }
+  }
+}
+
+// ----------------------------------------------------------
+window.addEventListener('scroll',left);
+
+function left(){
+  var lefts = document.querySelectorAll('.left');
+
+  for(var l = 0; l < lefts.length ; l++){
+
+    let windowheight = window.innerHeight;
+    let revealtopp = lefts[l].getBoundingClientRect().top;
+    let revealpointt = 100;
+
+    if(revealtopp < windowheight - revealpointt){
+      lefts[l].classList.add('active');
+    }
+    else{
+      lefts[l].classList.remove('active');
+    }
+  }
+}
+
+// ----------------------------------------------------------
+
+
+window.addEventListener('scroll',right);
+
+function right(){
+  var rights = document.querySelectorAll('.right');
+  
+  for(var r = 0; r < rights.length ; r++){
+    
+    let windowheight = window.innerHeight;
+    let revealtoppp = rights[r].getBoundingClientRect().top;
+    let revealpointtt = 100;
+    
+    if(revealtoppp < windowheight - revealpointtt){
+      rights[r].classList.add('active');
+    }
+    else{
+      rights[r].classList.remove('active');
+    }
+  }
+}
+// ----------------------------------------------------------
+
+// let arr = document.getElementById('arrow');
+
+// window.onscroll = function(){
+//     if(scrollY >= 740)
+//     {
+//         arr.style.display ='block';
+//     }else{
+//         arr.style.display ='none';
+//     }
+// }
+
+// -----------------------------------------------------------------
+// -----------------------------------------------------------------
+// -----------------------------------------------------------------
+
+
+// let first = document.getElementsByClassName('first');
+// let second = document.getElementsByClassName('second');
+// let third = document.getElementsByClassName('third');
+// let fourth = document.getElementsByClassName('fourth');
+// let card1 = document.getElementsByClassName('card1');
+// let card2 = document.getElementsByClassName('card2');
+// let card3 = document.getElementsByClassName('card3');
+// let card4 = document.getElementsByClassName('card4');
+
+
+
+
+
+
+// for(let a=0 ; a<first.length; a++){
+//     first[a].onclick = function(){
+//         card1[a].style.display = 'flex';  
+//         card2[a].style.display = 'none';  
+//         card3[a].style.display = 'none';  
+//         card4[a].style.display = 'none';  
+// }
+// }
+// for(let b=0 ; b<first.length; b++){
+//     second[b].onclick = function(){
+//         card2[b].style.display = 'flex';  
+//         card1[b].style.display = 'none';    
+//         card3[b].style.display = 'none';  
+//         card4[b].style.display = 'none';  
+// }
+// }
+// for(let c=0 ; c<first.length; c++){
+//     third[c].onclick = function(){
+//         card3[c].style.display = 'flex';  
+//         card1[c].style.display = 'none';  
+//         card2[c].style.display = 'none';  
+//         card4[c].style.display = 'none';  
+// }
+// }
+// for(let d=0 ; d<first.length; d++){
+//     fourth[d].onclick = function(){
+//         card4[d].style.display = 'flex';  
+//         card1[d].style.display = 'none';  
+//         card2[d].style.display = 'none';  
+//         card3[d].style.display = 'none';  
+// }
+// }
+
+
+let trailer1 = document.getElementsByClassName('trailer1');
+let trailer2 = document.getElementsByClassName('trailer2');
+let trailer3 = document.getElementsByClassName('trailer3');
+let trailer4 = document.getElementsByClassName('trailer4');
+let trailer5 = document.getElementsByClassName('trailer5');
+let trailer6 = document.getElementsByClassName('trailer6');
+let trailer7 = document.getElementsByClassName('trailer7');
+let trailer8 = document.getElementsByClassName('trailer8');
+let trailer9 = document.getElementsByClassName('trailer9');
+let trailer10 = document.getElementsByClassName('trailer10');
+let trailer11 = document.getElementsByClassName('trailer11');
+let trailer12 = document.getElementsByClassName('trailer12');
+let trailer13 = document.getElementsByClassName('trailer13');
+
+let btn1 = document.getElementsByClassName('btn1');
+let btn2 = document.getElementsByClassName('btn2');
+let btn3 = document.getElementsByClassName('btn3');
+let btn4 = document.getElementsByClassName('btn4');
+let btn5 = document.getElementsByClassName('btn5');
+let btn6 = document.getElementsByClassName('btn6');
+let btn7 = document.getElementsByClassName('btn7');
+let btn8 = document.getElementsByClassName('btn8');
+let btn9 = document.getElementsByClassName('btn9');
+let btn10 = document.getElementsByClassName('btn10');
+let btn11 = document.getElementsByClassName('btn11');
+let btn12 = document.getElementsByClassName('btn12');
+let btn13 = document.getElementsByClassName('btn13');
+
+
+let hide1 = document.getElementsByClassName('hide1');
+let hide2 = document.getElementsByClassName('hide2');
+let hide3 = document.getElementsByClassName('hide3');
+let hide4 = document.getElementsByClassName('hide4');
+let hide5 = document.getElementsByClassName('hide5');
+let hide6 = document.getElementsByClassName('hide6');
+let hide7 = document.getElementsByClassName('hide7');
+let hide8 = document.getElementsByClassName('hide8');
+let hide9 = document.getElementsByClassName('hide9');
+let hide10 = document.getElementsByClassName('hide10');
+let hide11 = document.getElementsByClassName('hide11');
+let hide12 = document.getElementsByClassName('hide12');
+let hide13 = document.getElementsByClassName('hide13');
+
+
+
+
+
+
+
+
+for(let t1=0 ; t1<btn1.length; t1++){
+  btn1[t1].onclick = function(){ 
+      trailer1[t1].style.opacity = '1';  
+}
+}
+for(let t2=0 ; t2<btn2.length; t2++){
+  btn2[t2].onclick = function(){ 
+      trailer2[t2].style.opacity = '1';  
+}
+}
+for(let t3=0 ; t3<btn3.length; t3++){
+  btn3[t3].onclick = function(){ 
+      trailer3[t3].style.opacity = '1';  
+}
+}
+for(let t4=0 ; t4<btn4.length; t4++){
+  btn4[t4].onclick = function(){ 
+      trailer4[t4].style.opacity = '1';  
+}
+}
+for(let t5=0 ; t5<btn5.length; t5++){
+  btn5[t5].onclick = function(){ 
+      trailer5[t5].style.opacity = '1';  
+}
+}
+for(let t6=0 ; t6<btn6.length; t6++){
+  btn6[t6].onclick = function(){ 
+      trailer6[t6].style.opacity = '1';  
+}
+}
+for(let t7=0 ; t7<btn7.length; t7++){
+  btn7[t7].onclick = function(){ 
+      trailer7[t7].style.opacity = '1';  
+}
+}
+for(let t8=0 ; t8<btn8.length; t8++){
+  btn8[t8].onclick = function(){ 
+      trailer8[t8].style.opacity = '1';  
+}
+}
+for(let t9=0 ; t9<btn9.length; t9++){
+  btn9[t9].onclick = function(){ 
+      trailer9[t9].style.opacity = '1';  
+}
+}
+for(let t10=0 ; t10<btn10.length; t10++){
+  btn10[t10].onclick = function(){ 
+      trailer10[t10].style.opacity = '1';  
+}
+}
+for(let t11=0 ; t11<btn11.length; t11++){
+  btn11[t11].onclick = function(){ 
+      trailer11[t11].style.opacity = '1';  
+}
+}
+for(let t12=0 ; t12<btn12.length; t12++){
+  btn12[t12].onclick = function(){ 
+      trailer12[t12].style.opacity = '1';  
+}
+}
+for(let t13=0 ; t13<btn13.length; t13++){
+  btn13[t13].onclick = function(){ 
+      trailer13[t13].style.opacity = '1';  
+}
+}
+
+// ----------------------------------------------------------------
+
+
+for(let h1=0 ; h1<hide1.length; h1++){
+  hide1[h1].onclick = function(){ 
+      trailer1[h1].style.opacity = '0';  
+}
+}
+for(let h2=0 ; h2<hide2.length; h2++){
+  hide2[h2].onclick = function(){ 
+      trailer2[h2].style.opacity = '0';  
+}
+}
+for(let h3=0 ; h3<hide3.length; h3++){
+  hide3[h3].onclick = function(){ 
+      trailer3[h3].style.opacity = '0';  
+}
+}
+for(let h4=0 ; h4<hide4.length; h4++){
+  hide4[h4].onclick = function(){ 
+      trailer4[h4].style.opacity = '0';  
+}
+}
+for(let h5=0 ; h5<hide5.length; h5++){
+  hide5[h5].onclick = function(){ 
+      trailer5[h5].style.opacity = '0';  
+}
+}
+for(let h6=0 ; h6<hide6.length; h6++){
+  hide6[h6].onclick = function(){ 
+      trailer6[h6].style.opacity = '0';  
+}
+}
+for(let h7=0 ; h7<hide7.length; h7++){
+  hide7[h7].onclick = function(){ 
+      trailer7[h7].style.opacity = '0';  
+}
+}
+for(let h8=0 ; h8<hide8.length; h8++){
+  hide8[h8].onclick = function(){ 
+      trailer8[h8].style.opacity = '0';  
+}
+}
+for(let h9=0 ; h9<hide9.length; h9++){
+  hide9[h9].onclick = function(){ 
+      trailer9[h9].style.opacity = '0';  
+}
+}
+for(let h10=0 ; h10<hide10.length; h10++){
+  hide10[h10].onclick = function(){ 
+      trailer10[h10].style.opacity = '0';  
+}
+}
+for(let h11=0 ; h11<hide11.length; h11++){
+  hide11[h11].onclick = function(){ 
+      trailer11[h11].style.opacity = '0';  
+}
+}
+for(let h12=0 ; h12<hide12.length; h12++){
+  hide12[h12].onclick = function(){ 
+      trailer12[h12].style.opacity = '0';  
+}
+}
+for(let h13=0 ; h13<hide13.length; h13++){
+  hide13[h13].onclick = function(){ 
+      trailer13[h13].style.opacity = '0';  
+}
+}
+
+// -----------------------------------------------------------
+// -----------------------------------------------------------
+// -----------------------------------------------------------
+// let next = document.querySelector('.next')
+// let prev = document.querySelector('.prev')
+
+// next.addEventListener('click', function(){
+//     let items = document.querySelectorAll('.item')
+//     document.querySelector('.slide').appendChild(items[0])
+// })
+
+// prev.addEventListener('click', function(){
+//     let items = document.querySelectorAll('.item')
+//     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+// })
+
+
+// --------------------------------------------------------------------
